50ecba349024a91158fc0cf7d2e3a43da3d5d5b3
Piperja <70609909+Piperja@users.noreply.github.com>
2024-04-10 01:01:59 +0000

    screaming

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-// Add JavaScript below
+function getData() {
+    fetch('https://jsonplaceholder.typicode.com/users')
+  .then(res => {
+    return res.json();
+  })
+  .then(data => {
+    data.forEach(user => {
+        const markup = `<li>${user.name}</li>`
+
+        document.querySelector(`ul`).insertAdjacentHTML(`beforeend`, markup);
+    })
+  })
+  
+}
